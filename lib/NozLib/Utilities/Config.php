c459f3d1c2f566c06f91362b6c487df0aa3de55d
Nozemi <nozemi95@gmail.com>
2017-02-09 15:07:53 +0100

    Yes.

--- lib/NozLib/Utilities/Config.php
+++ lib/NozLib/Utilities/Config.php
@@ -11,14 +11,14 @@
       $this->configDirectory = MISC::findFile($cnfDir); // Finds the config directory.
 
       // Checks and handles the error upon config directory not existing.
-      if(!file_exists($this->$configDirectory)) {
+      if(!file_exists($this->configDirectory)) {
         $this->lastError = 'Config directory wasn\'t found.';
         return false;
       }
 
       // Loads all the configs into an array.
       $this->config = array();
-      foreach(glob($this->$configDirectory . '*.conf.json') as $file) {
+      foreach(glob($this->configDirectory . '*.conf.json') as $file) {
         $this->config[basename($file,'.conf.json')] = json_decode(file_get_contents($file), true);
       }
     }
