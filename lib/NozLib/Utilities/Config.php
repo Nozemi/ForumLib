d21b56fd98861ac0b03c8c3ebbc61970cc499989
Nozemi <nozemi95@gmail.com>
2017-02-09 16:20:13 +0100

    Added config security check.

--- lib/NozLib/Utilities/Config.php
+++ lib/NozLib/Utilities/Config.php
@@ -7,6 +7,12 @@
 
     private $lastError;
 
+    /*
+      Config object adds all the configuration variables from the config directory's
+      config files. These files needs to be json, and end with .conf.json. For example: main.conf.json
+
+      Where you put the config directory doesn't matter. You can specify it's path in the constructor.
+    */
     public function __construct($cnfDir = 'config') {
       $this->configDirectory = MISC::findFile($cnfDir); // Finds the config directory.
 
@@ -16,10 +22,37 @@
         return false;
       }
 
+      // Let's verify that the config folder has a .htacccess file, and that it blocks any remote
+      // connections to access the config files. This is extremely critical to security, at least
+      // if database credentials and info is stored in those.
+      $this->ensureSecureConfigs();
+
       // Loads all the configs into an array.
       $this->config = array();
       foreach(glob($this->configDirectory . '/*.conf.json') as $file) {
         $this->config[basename($file,'.conf.json')] = json_decode(file_get_contents($file), true);
+      }
+    }
+
+    private function ensureSecureConfigs() {
+      // Let's check whether or not the .htaccess file is present.
+      if(!file_exists($this->configDirectory . '/.htaccess')) {
+        try {
+          // Open and write to the file if it doesn't exist.
+          $accessFile = fopen($this->configDirectory . "/.htaccess", "w");
+          $text = "Order deny,allow\nDeny from all\nAllow from 127.0.0.1";
+          fwrite($accessFile, $text);
+          fclose($accessFile);
+        } catch(Exception $ex) {
+          // Catch the error (if any) when attempting to create the file.
+          if(defined('DEBUG')) {
+            $this->lastError = $ex->getMessage();
+            return false;
+          } else {
+            $this->lastError = 'Something went wrong during the config loading.';
+            return false;
+          }
+        }
       }
     }
 
