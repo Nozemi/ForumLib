6881dc5c0e4e4948ddf955c5fb3b1ccea3bb9575
Nozemi <nozemi95@gmail.com>
2017-02-09 22:43:48 +0100

    Rework & Rename. Forum Library OOP

--- lib/ForumLib/Forums/Category.php
+++ lib/ForumLib/Forums/Category.php
@@ -2,5 +2,17 @@
   namespace ForumLib\Forums;
 
   class Category {
+    public $title;
+    public $description;
+    public $order;
+    public $enabled;
 
+    private $S;
+
+    private $lastError;
+    private $lastMessage;
+
+    public function __construct() {
+
+    }
   }
